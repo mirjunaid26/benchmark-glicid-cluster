f7601a5563e54d5060e26eb28be4b55faecfebda
Mir Junaid <mir.jnd@gmail.com>
2024-01-05 11:03:33 +0000

    add

--- visualize_gradient.py
+++ visualize_gradient.py
@@ -11,3 +11,36 @@ np.random.seed(42)
 x = np.random.rand(N, 1)
 epsilon = (.1 * np.random.randn(N, 1))
 y = true_b + true_w + epsilon
+
+# Shuffles the indices
+idx = np.arange(N)
+np.random.shuffle(idx)
+
+# Uses first 80 random indices for train
+train_idx = idx[:int(N*.8)]
+# Uses the remaining indices for validation
+val_idx = idx[int(N*.8):]
+
+# Generates train and validation sets
+x_train, y_train = x[train_idx], y[train_idx]
+x_val, y_val = x[val_idx], y[val_idx]
+
+# Step 0 - Initializes parameters "b" and "w" randomly
+np.random.seed(42)
+b = np.random.randn(1)
+w = np.random.randn(1)
+
+print(b, w)
+
+# Step 1: Compute Model's Predictions
+# Step 1 - Computes our model's predicted output - forward pass
+yhat = b + w * x_train
+
+# Step 2 - Computing the loss
+# We are using ALL data points, so this is BATCH gradient
+# descent. How wrong is our model? That's the error!
+error = (yhat - y_train)
+
+# It is a regression, so it computes mean squared error (MSE)
+loss = (error ** 2).mean()
+print(loss)
