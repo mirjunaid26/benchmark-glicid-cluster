a9a8fe978e5774400617e11b6002827dd68e17f0
jmir <junaid.mir@ec-nantes.fr>
2024-01-05 11:58:01 +0100

    add

---
+++ visualize_gradient.py
@@ -0,0 +1,13 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
+
+true_b = 1
+true_w = 2
+N = 100
+
+# Data Generation
+np.random.seed(42)
+x = np.random.rand(N, 1)
+epsilon = (.1 * np.random.randn(N, 1))
+y = true_b + true_w + epsilon
